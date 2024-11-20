70eaf748026c04e03ed71a7c028f50adf6966a0c
LeakyMon <hmonforte17@gmail.com>
2024-11-20 16:54:26 -0500

    Added the unit tests in Counter.test.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,42 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+// imports, include the counter 
+import { render, screen } from '@testing-library/react';
+import Counter from '../components/Counter'; // Adjust the path if necessary
+import userEvent from '@testing-library/user-event';
 
+
+//same thing kind of as app.test.js but render the counter
 beforeEach(() => {
-  // Render the Counter component here
-})
+  render(<Counter />);
+});
 
+//make sure message renders
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const message = screen.getByText(/Counter/i); 
+  expect(message).toBeInTheDocument();
 });
 
+//check init count is 0
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const countValue = screen.getByTestId('count'); 
+  expect(countValue).toHaveTextContent('0');
 });
 
+
+// testing the plus button to increment count
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const plusButton = screen.getByText('+'); 
+  const countValue = screen.getByTestId('count'); 
+
+  userEvent.click(plusButton);
+  expect(countValue).toHaveTextContent('1');
 });
 
+//now testing the minus
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const minusButton = screen.getByText('-'); 
+
+  const countValue = screen.getByTestId('count'); 
+
+  userEvent.click(minusButton);
+  expect(countValue).toHaveTextContent('-1'); 
 });
